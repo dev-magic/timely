1ec01ae522154559ff503e188c62a5fef989033e
Gabriel Bonner <gabrielbonner@gmail.com>
2017-08-28 22:44:07 -0700

    Add tests for models

--- app/models/timeslot.rb
+++ app/models/timeslot.rb
@@ -1,5 +1,7 @@
 # A potential time for an event
 class Timeslot < ApplicationRecord
+  validates_presence_of :start_time, :event_id
+
   belongs_to :event
-  has_many :preferences
+  has_many :preferences, class_name: "Event"
 end
--- spec/models/timeslot_spec.rb
+++ spec/models/timeslot_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Timeslot, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:event) { Event.create(name: "dev magic", time: DateTime.now, duration_minutes: 5, id: 1)}
+
+  subject {
+    described_class.new(start_time: DateTime.now, event_id: 1)
+  }
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is invalid without a start_time" do
+    subject.start_time = nil
+    expect(subject).to_not be_valid
+  end
+
+  describe "Associations" do
+    it { should belong_to(:event) }
+    it { should have_many(:preferences) }
+  end
+
+  describe "Associations" do
+    it "belongs to event" do
+      assc = described_class.reflect_on_association(:event)
+      expect(assc.macro).to eq :belongs_to
+    end
+
+    it "has many preferences" do
+      assc = described_class.reflect_on_association(:preferences)
+      expect(assc.macro).to eq :has_many
+    end
+  end
 end
