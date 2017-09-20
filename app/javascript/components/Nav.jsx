ff895ed1688330e78e248d4fd313adba9d390e68
Kevin Brewer <appleJax@users.noreply.github.com>
2017-09-19 20:58:39 -0700

    Style signout (#41)
    Toggle Sign In / Sign Out based on current_user

--- app/javascript/components/Nav.jsx
+++ app/javascript/components/Nav.jsx
@@ -1,64 +1,93 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Nav = ({ authToken, currentUser }) => {
-  const signInButtons = (
-    <div>
-      <a href={"/users/sign_up"}>
-        <button className='btn'>
-          Sign Up
-        </button>
-      </a>
-      <a href={"/users/sign_in"}>
-        <button className='btn'>
-          Sign In
-        </button>
-      </a>
-    </div>
-  )
+class Nav extends Component {
+  constructor(props) {
+    super(props)
 
-  const signOutButton = (
-    <div className='greeting-bar'>
-      <i className='fa fa-user greeting'/>
-      <p className='greeting'>
-        Hello, { currentUser ? currentUser.name : '' }!
-      </p>
-      <form
-        method='POST'
-        action="/users/sign_out"
-      >
-        <input
-          type='hidden'
-          name='_method'
-          value='delete'
-        />
-        <input
-          type='hidden'
-          name='authenticity_token'
-          value={ authToken }
-        />
-        <input
-          type='submit'
-          value='Sign Out'
-          className='sign-out btn'
-        />
-      </form>
-    </div>
-  )
+    this.state = {
+      showSignout: false
+    }
 
-  return (
-    <nav className='navbar'>
-      <div className='logo'>
-        <a href='/'>Let's Meet Up!</a>
+    this.toggleSignout = this.toggleSignout.bind(this)
+  }
+
+  toggleSignout() {
+    this.setState({ showSignout: !this.state.showSignout })
+  }
+
+  render() {
+    const { authToken, currentUser } = this.props
+
+    const signInButtons = (
+      <div>
+        <a href={"/users/sign_up"}>
+          <button className='btn'>
+            Sign Up
+          </button>
+        </a>
+        <a href={"/users/sign_in"}>
+          <button className='btn'>
+            Sign In
+          </button>
+        </a>
       </div>
+    )
 
-      <div className='auth-bar'>
-      { currentUser
-      ? signOutButton
-      : signInButtons
-      }
+    const toggleArrow = this.state.showSignout ? 'fa-sort-up' : 'fa-sort-down'
+    const showSignout = this.state.showSignout ? 'show-signout' : ''
+
+    const signOutButton = (
+      <div className='greeting-bar'>
+        <div
+          className='greeting'
+          onClick={ this.toggleSignout }
+        >
+          <i className='fa fa-user'/>
+          <p className='greeting-msg'>
+            Hello, { currentUser ? currentUser.name : '' }!
+          </p>
+          <i className={`fa ${toggleArrow} signout-arrow`}
+          />
+        </div>
+        <form
+          method='POST'
+          action='/users/sign_out'
+          className={`signout-form ${showSignout}`}
+        >
+          <input
+            type='hidden'
+            name='_method'
+            value='delete'
+          />
+          <input
+            type='hidden'
+            name='authenticity_token'
+            value={ authToken }
+          />
+          <input
+            type='submit'
+            value='Sign Out'
+            className='signout'
+          />
+        </form>
       </div>
-    </nav>
-  )
+    )
+
+    return (
+      <nav className='navbar'>
+        <div className='logo'>
+          <a href='/'>Let's Meet Up!</a>
+        </div>
+
+        <div className='auth-bar'>
+        { currentUser
+        ? signOutButton
+        : signInButtons
+        }
+        </div>
+      </nav>
+    )
+  }
 }
 
  export default Nav
---
+++ spec/features/users_signout_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+RSpec.describe "Users can sign out", js: true do
+
+  let(:user) { User.create(name: "dev magic", email: "me@example.com", password: "password") }
+
+  before(:each) do
+    visit new_user_session_path
+
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+
+    within '.actions' do
+      click_button "Log in"
+    end
+  end
+
+  scenario "signout from root path" do
+
+    expect(page).to have_current_path root_path
+    expect(page).to_not have_content "Sign In"
+    expect(page).to_not have_content "Sign Up"
+    expect(page).to have_content "Hello, dev magic!"
+
+    find(".greeting").click
+
+    click_on "Sign Out"
+
+    expect(page).to have_current_path root_path
+    expect(page).to have_content "Sign In"
+    expect(page).to have_content "Sign Up"
+
+  end
+
+  scenario "signout from /events path" do
+
+    visit events_path
+
+    expect(page).to_not have_content "Sign In"
+    expect(page).to_not have_content "Sign Up"
+    expect(page).to have_content "Hello, dev magic!"
+
+    find(".greeting").click
+
+    click_on "Sign Out"
+
+    expect(page).to have_current_path root_path
+    expect(page).to have_content "Sign In"
+    expect(page).to have_content "Sign Up"
+
+  end
+end
