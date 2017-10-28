ff549d9ce877b391b5cfe7a122f8f800f99991a1
appleJax <appleJax@users.noreply.github.com>
2017-10-28 14:54:05 -0700

    Fix hound issues

--- app/models/event.rb
+++ app/models/event.rb
@@ -20,45 +20,50 @@ class Event < ApplicationRecord
     end
   end
 
-def add_best_timeslot
-  ranked_timeslots = calculate_timeslot_ranking
-  best_timeslot = ranked_timeslots.select { |ts| ts.rank == 1 }
+  def add_best_timeslot
+    ranked_timeslots = calculate_timeslot_ranking
+    best_timeslot = ranked_timeslots.select { |ts| ts.rank == 1 }
 
-  define_singleton_method(:best_timeslot) { best_timeslot[0].start_time }
+    define_singleton_method(:best_timeslot) { best_timeslot[0].start_time }
 
-  self
-end
+    self
+  end
+
+  def preference_score(preference)
+    case preference
+    when 'tentative'
+      1
+    when 'available'
+      2
+    when 'preferred'
+      3
+    when 'not_available'
+      -100
+    else
+      0
+    end
+  end
 
   def calculate_timeslot_ranking
-    timeslots_with_score = self.timeslots.map do |timeslot|
+    timeslots_with_score = timeslots.map do |timeslot|
       score = timeslot.preferences.reduce(0) do |sum, pref|
-        case pref.preference_type
-        when 'tentative'
-          sum += 1
-        when 'available'
-          sum += 2
-        when 'preferred'
-          sum += 3
-        when 'not_available'
-          sum -= 100
-        else
-          # do nothing
-        end
+        sum += preference_score pref.preference_type
         sum
       end
 
       timeslot.define_singleton_method(:score) { score }
+      timeslot.define_singleton_method(:rank) { nil }
       timeslot
     end
 
-    timeslots_with_score.sort do |a, b|
+    sorted_timeslots = timeslots_with_score.sort do |a, b|
       b.score <=> a.score
-    end.each_with_index.map do |timeslot, i|
+    end
+
+    sorted_timeslots.each_with_index.map do |timeslot, i|
       if i < 3
         ranking = 1 + i
         timeslot.define_singleton_method(:rank) { ranking }
-      else
-        timeslot.define_singleton_method(:rank) { nil }
       end
       timeslot
     end
