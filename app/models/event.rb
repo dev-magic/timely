f8d9c31ab0e07e8a8a31e3921a68709036b7a003
appleJax <appleJax@users.noreply.github.com>
2017-11-01 22:45:39 -0700

    Disable best_timeslot if no preferences chosen

--- app/models/event.rb
+++ app/models/event.rb
@@ -22,6 +22,9 @@ class Event < ApplicationRecord
 
   def best_timeslot
     ranked_timeslots = timeslots_with_ranking
+
+    return nil if timeslots_with_ranking.all? { |ts| ts.score == 0 }
+
     best_timeslot = ranked_timeslots.select { |ts| ts.rank == 1 }
     best_timeslot[0].start_time
   end
@@ -48,6 +51,8 @@ class Event < ApplicationRecord
   end
 
   def add_rank_to_timeslots(timeslot_array)
+    return timeslot_array if timeslot_array.all? { |ts| ts.score == 0 }
+
     timeslot_array.each_with_index.map do |timeslot, i|
       if i < 3
         ranking = 1 + i
--- app/serializers/event_with_best_timeslot_serializer.rb
+++
@@ -1,7 +0,0 @@
-class EventWithBestTimeslotSerializer < EventSerializer
-  attributes :best_timeslot
-
-  def best_timeslot
-    object.best_timeslot
-  end
-end
